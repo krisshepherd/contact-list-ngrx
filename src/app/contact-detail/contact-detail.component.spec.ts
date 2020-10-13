10ec744a85afaf92440531cec02d7e8dc5312b49
Kristof Juhasz <kristof.juhasz.asap@gmail.com>
2020-10-13 14:16:08 +0200

    Contact-detail component unit tests

--- src/app/contact-detail/contact-detail.component.spec.ts
+++ src/app/contact-detail/contact-detail.component.spec.ts
@@ -1,25 +1,38 @@
+import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { CONTACTS } from '../store/mock-contacts';
 
 import { ContactDetailComponent } from './contact-detail.component';
 
-describe('ContactDetailComponent', () => {
+fdescribe('ContactDetailComponent', () => {
   let component: ContactDetailComponent;
   let fixture: ComponentFixture<ContactDetailComponent>;
+  let contact = CONTACTS[0];
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ ContactDetailComponent ]
     })
-    .compileComponents();
+    .compileComponents()
+    .then(()=> {
+      fixture = TestBed.createComponent(ContactDetailComponent);
+      component = fixture.componentInstance;
+    });
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ContactDetailComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
   it('should create', () => {
+    expect(component).toBeDefined();
+    component.contact = contact;
+    fixture.detectChanges();
     expect(component).toBeTruthy();
   });
+
+  it('should display the first name of the contact', ()=> {
+    component.contact = contact;
+    fixture.detectChanges();
+    const firstname = fixture.debugElement.query(By.css("p:first-child")).nativeElement;
+    expect(firstname.innerHTML).toBe("First Name: " + contact.firstname);
+  });
+
 });
